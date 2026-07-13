1898e8904691cf1115cc1e8c85353f538c6cbd04
ayoubtaoufikofficial <ayoubtaoufikofficial@gmail.com>
2026-07-13 17:47:45 +0100

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css' // ila 3ndk chi CSS global
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
